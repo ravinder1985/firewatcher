f8d453c4a52d4bd8ff0244c26d58fd5e9a85075e
Harvinder Singh <harvindersingh@Harvinders-MacBook-Pro.local>
2017-11-22 19:12:52 -0500

    Allow user to run custom scrits to monitor nodes

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/firewatcher/system"
+)
+
+var data system.Data
+var jsonConfig system.JSON
+
+func metrics(w http.ResponseWriter, r *http.Request) {
+	var d, s string
+	for _, commands := range jsonConfig.Commands {
+		name := commands.Name + "_" + commands.Type
+		s = "# HELP " + name + " check status of the service 0 = OK | 1 = WARNING | 2 = CRITICAL | 3 = UNKNOWN\n"
+		s = s + "# TYPE " + name + " Untyped"
+		d += s + "\n"
+		//name := commands.Name
+
+		switch strings.TrimSpace(string(data.Result[commands.Name])) {
+		case "0":
+			fmt.Println("OK")
+			d += name + `{type="` + commands.Lables.Type + `",status="OK"} ` + string(data.Result[commands.Name]) + ``
+		case "1":
+			fmt.Println("WARNING")
+			d += name + `{type="` + commands.Lables.Type + `",status="WARNING"} ` + string(data.Result[commands.Name]) + ``
+		case "2":
+			fmt.Println("CRITICAL")
+			d += name + `{type="` + commands.Lables.Type + `",status="CRITICAL"} ` + string(data.Result[commands.Name]) + ``
+		default:
+			fmt.Println("UNKNOWN")
+			d += name + `{type="` + commands.Lables.Type + `",status="UNKNOWN"} ` + string(data.Result[commands.Name]) + ``
+		}
+	}
+	fmt.Fprintf(w, d)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<a href=/metrics>View Matrics</a>")
+}
+
+func parseConfig() {
+	pwd, _ := os.Getwd()
+	var filename = "/config.json"
+	source, err := ioutil.ReadFile(pwd + filename)
+	//fmt.Printf("Value: %s", source)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(source, &jsonConfig)
+	//
+	// fmt.Println(jsonConfig.Duration)
+	// for _, commands := range jsonConfig.Commands {
+	// 	fmt.Println(commands.Name)
+	// }
+}
+
+func main() {
+	parseConfig()
+	go system.Poll(&data, jsonConfig)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/metrics", metrics)
+	http.ListenAndServe(":"+jsonConfig.Port, nil)
+}
---
+++ system/commands.go
@@ -0,0 +1,81 @@
+package system
+
+import (
+	"log"
+	"os/exec"
+	"time"
+)
+
+//ConfigJSON object
+type ConfigJSON struct {
+	Name    string `json:"name"`
+	Command string `json:"command"`
+	Options string `json:"options"`
+}
+
+// JSON has json object
+type JSON struct {
+	Duration int
+	Port     string
+	Commands []Commands
+}
+
+// Commands has commands onject
+type Commands struct {
+	Name    string   `json:"name"`
+	Type    string   `json:"type"`
+	Command string   `json:"command"`
+	Options []string `json:"options"`
+	Lables  Lables   `json:"lables"`
+}
+
+// Lables has leveles for commands
+type Lables struct {
+	Type string `json:"type"`
+}
+
+// Data would hole the data
+type Data struct {
+	Result map[string][]byte
+}
+
+var jsonConfig JSON
+
+// Poll would parse configs and run them every interval.
+func Poll(data *Data, jsonConfig JSON) {
+	duration := jsonConfig.Duration
+	go ConfigCommand(jsonConfig, data)
+	for {
+		<-time.After(time.Duration(duration) * time.Second)
+		go ConfigCommand(jsonConfig, data)
+	}
+}
+
+// ConfigCommand Run command from config file
+func ConfigCommand(jsonConfig JSON, data *Data) {
+	for _, commands := range jsonConfig.Commands {
+		name := commands.Name
+		command := commands.Command
+		options := commands.Options
+		if data.Result == nil {
+			//fmt.Println("Allocate memory")
+			data.Result = make(map[string][]byte, 1)
+		}
+		if options == nil {
+			out, err := exec.Command(command).Output()
+			if (err) != nil {
+				log.Fatal(err)
+			}
+			data.Result[name] = out
+			//fmt.Printf("%s: %s", name, out)
+		} else {
+			//fmt.Println(options)
+			out, err := exec.Command(command, options...).Output()
+			if (err) != nil {
+				log.Fatal(err)
+			}
+			data.Result[name] = out
+			//fmt.Printf("%s: %s", name, out)
+		}
+	}
+}
